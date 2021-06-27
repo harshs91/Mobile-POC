f95d51cf70c1ab5ffb517b8e107e2e358dfcf367
Harsh Sorathiya <harsh.sorathiya@gmail.com>
2021-06-27 18:50:07 +0530

    Added in-memory authentication and role based authorization for actuator endpoints.

---
+++ src/main/java/com/mobile/poc/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.mobile.poc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder.encode("userpass"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("adminpass"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/actuator/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
